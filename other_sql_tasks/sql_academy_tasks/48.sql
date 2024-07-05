c35e7e8222ba974164bf90b10d52fa4bb221799a
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-07-06 02:51:19 +0300

    48 task

---
+++ other_sql_tasks/sql_academy_tasks/48.sql
@@ -0,0 +1,7 @@
+ -- https://sql-academy.org/ru/trainer/tasks/48
+
+SELECT name, COUNT(*) as count FROM Student_in_class
+JOIN Class
+    ON Class.id = Student_in_class.class
+GROUP BY name
+ORDER BY count DESC
