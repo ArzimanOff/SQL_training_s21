e7ca2f5566f869761fea35efe2dbeaa98c17baff
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-07-11 02:11:53 +0300

    53-54 tasks

---
+++ other_sql_tasks/sql_academy_tasks/53.sql
@@ -0,0 +1,5 @@
+-- https://sql-academy.org/ru/trainer/tasks/53
+
+UPDATE FamilyMembers 
+SET member_name = "Andie Anthony"
+WHERE member_name = "Andie Quincey";
---
+++ other_sql_tasks/sql_academy_tasks/54.sql
@@ -0,0 +1,4 @@
+-- https://sql-academy.org/ru/trainer/tasks/54
+
+DELETE FROM FamilyMembers
+WHERE member_name LIKE "%Quincey"
