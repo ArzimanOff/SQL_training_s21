8c909514a307e5832abb652e861e910c6fabb164
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-07-13 01:46:36 +0300

    58 upd

--- other_sql_tasks/sql_academy_tasks/58.sql
+++ other_sql_tasks/sql_academy_tasks/58.sql
@@ -1,20 +1,19 @@
 -- https://sql-academy.org/ru/trainer/tasks/58
 
-WITH 
-need_reservation AS (
-    SELECT Reservations.id as id FROM Reviews
-    RIGHT JOIN Reservations
-        ON Reservations.id = Reviews.reservation_id 
-    JOIN Rooms
-        ON Rooms.id = Reservations.room_id
-    JOIN Users 
-        ON Users.id = Reservations.user_id
-    WHERE reservation_id IS NULL AND Users.name = 'George Clooney'
-)
 
 INSERT INTO Reviews
 VALUES (
-    (SELECT COUNT(*) + 1 FROM Reviews),
-    (SELECT id FROM need_reservation), 
+    (SELECT COUNT(*) + 1 FROM Reviews as rw), 
+    -- без 'as rw' не работает, так как нельзя считать кол-во полей в таблице 
+    -- и одновременно в эту таблицу что-то добавлять
+    (
+        SELECT Reservations.id FROM  Reservations
+        JOIN Rooms
+            ON Rooms.id = Reservations.room_id
+        JOIN Users 
+            ON Users.id = Reservations.user_id
+        WHERE address = '11218, Friel Place, New York' 
+        AND name = 'George Clooney'
+    ), 
     5
 );
