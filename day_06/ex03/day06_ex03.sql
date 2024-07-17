06f4435a665e5b35f9925066248e7cc2f7a7abae
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-07-17 21:14:13 +0300

    day 06 ex 03

---
+++ day_06/ex03/day06_ex03.sql
@@ -0,0 +1,9 @@
+CREATE UNIQUE INDEX idx_person_discounts_unique
+    ON person_discounts(person_id, pizzeria_id);
+
+-- нужно выполнить в качестве примера работоспособности
+-- SET enable_seqscan = OFF;
+-- EXPLAIN (ANALYSE, TIMING OFF)
+-- SELECT * FROM person_discounts
+--          WHERE person_id = 1
+--            AND pizzeria_id = 1;
