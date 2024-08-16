6c1d2a56c881d9363101a140571c0fdd0bfbfe0c
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-08-16 19:27:27 +0300

    ex02-04

--- day_09/ex03/day09_ex03.sql
+++ day_09/ex03/day09_ex03.sql
@@ -1,7 +1,26 @@
-CREATE OR REPLACE FUNCTION fnc_trg_person_delete_audit()
+DROP TRIGGER IF EXISTS trg_person_insert_audit ON person;
+DROP FUNCTION IF EXISTS fnc_trg_person_insert_audit();
+
+DROP TRIGGER IF EXISTS trg_person_update_audit ON person;
+DROP FUNCTION IF EXISTS fnc_trg_person_update_audit();
+
+DROP TRIGGER IF EXISTS trg_person_delete_audit ON person;
+DROP FUNCTION IF EXISTS fnc_trg_person_delete_audit();
+
+
+CREATE OR REPLACE FUNCTION fnc_trg_person_audit()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF TG_OP = 'DELETE' THEN
+    IF 
+        TG_OP = 'INSERT' THEN
+        INSERT INTO person_audit (row_id, name, age, gender, address)
+        VALUES (NEW.id, NEW.name, NEW.age, NEW.gender, NEW.address);
+    ELSIF 
+        TG_OP = 'UPDATE' THEN
+        INSERT INTO person_audit (type_event, row_id, name, age, gender, address)
+        VALUES ('U', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
+    ELSIF 
+        TG_OP = 'DELETE' THEN
         INSERT INTO person_audit (type_event, row_id, name, age, gender, address)
         VALUES ('D', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
     END IF;
@@ -10,14 +29,26 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE TRIGGER trg_person_delete_audit
-    AFTER DELETE
+CREATE TRIGGER trg_person_audit
+    AFTER DELETE OR UPDATE OR INSERT
     ON person
     FOR EACH ROW
-EXECUTE FUNCTION fnc_trg_person_delete_audit();
+EXECUTE FUNCTION fnc_trg_person_audit();
 
-DELETE FROM person WHERE id = 10
+TRUNCATE TABLE person_audit;
 
+-- SELECT * FROM person;
+-- SELECT * FROM person_audit;
+INSERT INTO person(id, name, age, gender, address)
+VALUES (10, 'Damir', 22, 'male', 'Irkutsk');
+-- SELECT * FROM person;
+-- SELECT * FROM person_audit;
+
+UPDATE person SET name = 'Bulat' WHERE id = 10;
+UPDATE person SET name = 'Damir' WHERE id = 10;
+-- SELECT * FROM person;
+-- SELECT * FROM person_audit;
+
+DELETE FROM person WHERE id = 10;
 -- SELECT * FROM person;
 -- SELECT * FROM person_audit;
