5ab9e4fd0578a3b1301cf8757b985c0f93d11b8a
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-08-20 14:46:30 +0300

    ex01

--- team_01/ex01/day09_ex01.sql
+++ team_01/ex01/day09_ex01.sql
@@ -1,24 +1,47 @@
-CREATE OR REPLACE FUNCTION fnc_trg_person_update_audit()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF TG_OP = 'UPDATE' THEN
-        INSERT INTO person_audit (type_event, row_id, name, age, gender, address)
-        VALUES ('U', OLD.id, OLD.name, OLD.age, OLD.gender, OLD.address);
-    END IF;
+-- insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
+-- insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER trg_person_update_audit
-    AFTER UPDATE
-    ON person
-    FOR EACH ROW
-EXECUTE FUNCTION fnc_trg_person_update_audit();
-
-UPDATE person SET name = 'Bulat' WHERE id = 10;
-UPDATE person SET name = 'Damir' WHERE id = 10;
-
--- SELECT * FROM person;
--- SELECT * FROM person_audit;
+WITH 
+-- сохраняем уникальные id и названия валют
+unique_currency AS (
+	SELECT id, name 
+	FROM currency 
+	GROUP BY id, name
+),
+-- содержит данные о пользователе, название валюты, сумму денег (balance.money), 
+-- и ближайший курс валюты относительно даты обновления баланса
+nearest_rate AS (
+	SELECT 
+        u.name AS name,
+        u.lastname AS lastname,
+        uni_c.name AS currency_name,
+        balance.money,
+        COALESCE(
+            (
+                SELECT rate_to_usd
+                FROM currency
+                WHERE currency_id = currency.id AND currency.updated < balance.updated 
+                ORDER BY currency.updated DESC 
+                LIMIT 1
+            ), 
+            (
+                SELECT rate_to_usd
+                FROM currency
+                WHERE currency_id = currency.id AND currency.updated > balance.updated 
+                ORDER BY currency.updated ASC 
+                LIMIT 1
+            )
+        ) AS rate_to_usd
+    FROM balance
+    -- Это соединение добавляет к каждой записи из balance название валюты (currency_name)
+    INNER JOIN unique_currency AS uni_c ON uni_c.id = currency_id 
+    -- Добавляет информацию о пользователе (имя name и фамилия lastname) к каждой записи в balance
+    LEFT JOIN "user" u ON u.id = user_id
+) 
+SELECT 
+    COALESCE(name, 'not defined') AS name,
+    COALESCE(lastname, 'not defined') AS lastname, 
+    currency_name,  
+    money * rate_to_usd AS currency_in_usd
+FROM nearest_rate
+ORDER BY name DESC, lastname, currency_name;
